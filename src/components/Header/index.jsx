8c6c58efcb175e4a77c32ded6770e350d87d8076
Evaloper <gbajeeva@yahoo.com>
2023-11-03 23:23:57 +0100

    Hamburger added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,44 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaFacebookF, FaInstagram, FaLinkedinIn, FaBehance } from "react-icons/fa";
 import "./index.scss"
 
 const Header = () => {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const handleNavState = () => {
+        setIsNavOpen(!isNavOpen);
+    };
+
     return (
         <div className="header-div">
-            <div className='logo-div'><a href="/">&lt;Oghenefejiro /&gt;</a></div>
-            <nav>
-                <a href="/about"><span className="span">01.</span> About</a>
-                <a href="/services "><span className="span">02.</span> Portfolio</a>
-                <a href="/contact "><span className="span">03.</span> Contact</a>
-                <a href="/"><button className='btn'>Resume</button></a>
-            </nav>
-            <div class="icons-div">
-                <a href="#" className='icons fa-faceobok'><FaFacebookF /></a>
-                <a href="#" class="icons fa-instagram"><FaInstagram /></a>
-                <a href="#" class="icons fa-linkedin"><FaLinkedinIn /></a>
-                <a href="#" class="icons fa-behance"><FaBehance /></a>
+            <div className='desktop-view'>
+                <div className='logo-div'><a href="/">&lt;Oghenefejiro /&gt;</a></div>
+                <nav>
+                    <a href="/about"><span className="span">01.</span> About</a>
+                    <a href="/services "><span className="span">02.</span> Portfolio</a>
+                    <a href="/contact "><span className="span">03.</span> Contact</a>
+                    <a href="/"><button className='btn'>Resume</button></a>
+                </nav>
+                <div class="icons-div">
+                    <a href="#" className='icons fa-faceobok'><FaFacebookF /></a>
+                    <a href="#" class="icons fa-instagram"><FaInstagram /></a>
+                    <a href="#" class="icons fa-linkedin"><FaLinkedinIn /></a>
+                    <a href="#" class="icons fa-behance"><FaBehance /></a>
+                </div>
             </div>
-            {/* <div class="mobile-view">
+            <div class="mobile-view">
                 <div class="logo">
-                    <img src="/images/logo.png" class="logo-img" />
+                    <div className='logo-div'><a href="/">&lt;Oghenefejiro /&gt;</a></div>
                 </div>
-                <div class="hamburger">
-                    <div id="openNav">
-                        <img src="/images/hamburger-svgrepo-com.svg" />
+                <div>
+                    <div onClick={handleNavState} class="hamburger">
+                        <img src="/src/assets/hamburger-svgrepo-com.svg" alt="hamburger" />
                     </div>
-                    <div id="myNav" class="overlay">
-                        <div id="closeNav">
-                            <a href="javascript:void(0)" class="closebtn"> &times;</a>
+                    {isNavOpen && (
+                        <div id="myNav" className="overlay">
+                            <div>
+                                <a
+                                    href="javascript:void(0)"
+                                    className="closebtn"
+                                    onClick={handleNavState}
+                                >
+                                    &times;
+                                </a>
+                            </div>
+                            <div className="overlay-content">
+                                <a className="nav" href='/'>
+                                    About
+                                </a>
+                                <a href='/' className="nav">
+                                    Portfolio
+                                </a>
+                                <a href='/' className="nav">
+                                    Contact
+                                </a>
+                            </div>
                         </div>
-                        <div class="overlay-content" id="myNav">
-                            <a href="/">Home</a>
-                            <a href="/about">About</a>
-                            <a href="/services ">Services</a>
-                            <a href="/contact ">Contact</a>
-                        </div>
-                    </div>
+                    )}
                 </div>
-            </div> */}
+            </div>
         </div>
     )
 }
