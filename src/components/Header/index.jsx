b9f817a7f53d6153ecd7159d8b87fa6d5e9987af
Evaloper <gbajeeva@yahoo.com>
2023-11-03 20:02:58 +0100

    Mobile responsiveness for logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,19 +6,17 @@ const Header = () => {
     return (
         <div className="header-div">
             <div className='logo-div'><a href="/">&lt;Oghenefejiro /&gt;</a></div>
-            <div className="desktop-view">
-                <nav>
-                    <a href="/about"><span className="span">01.</span> About</a>
-                    <a href="/services "><span className="span">02.</span> Portfolio</a>
-                    <a href="/contact "><span className="span">03.</span> Contact</a>
-                    <a href="/"><button className='btn'>Resume</button></a>
-                </nav>
-                <div class="icons-div">
-                    <a href="#" className='icons fa-faceobok'><FaFacebookF /></a>
-                    <a href="#" class="icons fa-instagram"><FaInstagram /></a>
-                    <a href="#" class="icons fa-linkedin"><FaLinkedinIn /></a>
-                    <a href="#" class="icons fa-behance"><FaBehance /></a>
-                </div>
+            <nav>
+                <a href="/about"><span className="span">01.</span> About</a>
+                <a href="/services "><span className="span">02.</span> Portfolio</a>
+                <a href="/contact "><span className="span">03.</span> Contact</a>
+                <a href="/"><button className='btn'>Resume</button></a>
+            </nav>
+            <div class="icons-div">
+                <a href="#" className='icons fa-faceobok'><FaFacebookF /></a>
+                <a href="#" class="icons fa-instagram"><FaInstagram /></a>
+                <a href="#" class="icons fa-linkedin"><FaLinkedinIn /></a>
+                <a href="#" class="icons fa-behance"><FaBehance /></a>
             </div>
             {/* <div class="mobile-view">
                 <div class="logo">
